14cd40750850e4977300f1a50273dd97788292de
Sam Lahti <sam@gopangea.com>
2013-10-14 14:07:01 -0500

    Adding some priortization of SSL enabled cyphers rather than using default system implementation which tends to default more towards MD5 and RC4
    As conveyed in this article:
    http://op-co.de/blog/posts/android_ssl_downgrade/

--- okhttp/src/main/java/com/squareup/okhttp/Connection.java
+++ okhttp/src/main/java/com/squareup/okhttp/Connection.java
@@ -76,6 +76,23 @@ public final class Connection implements Closeable {
   private static final byte[] HTTP_11 = new byte[] {
       'h', 't', 't', 'p', '/', '1', '.', '1'
   };
+  
+  static final String ENABLED_CIPHERS[] = {
+       "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+       "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+       "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+       "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+       "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+       "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+       "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+       "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
+       "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
+       "TLS_RSA_WITH_AES_128_CBC_SHA",
+       "TLS_RSA_WITH_AES_256_CBC_SHA",
+       "SSL_RSA_WITH_3DES_EDE_CBC_SHA",
+       "SSL_RSA_WITH_RC4_128_SHA",
+       "SSL_RSA_WITH_RC4_128_MD5",
+   };
 
   private final Route route;
 
@@ -142,6 +159,8 @@ public final class Connection implements Closeable {
       platform.setNpnProtocols(sslSocket, NPN_PROTOCOLS);
     }
 
+    sslSocket.setEnabledCipherSuites(ENABLED_CIPHERS);
+    
     // Force handshake. This can throw!
     sslSocket.startHandshake();
 
